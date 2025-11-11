a2cb1f7ce00e852b6866ba78bcfbb9a2efb233df
beatriz0freitas <a106853@alunos.uminho.pt>
2025-11-11 08:51:43 +0000

    update classe evento

--- Evento.java
+++ Evento.java
@@ -3,12 +3,12 @@ import java.io.Serializable;
 public class Evento implements Serializable {
     private String nome_produto;
     private int quantidade;
-    private double preço_unitario;
+    private double preco_unitario;
     
-    public Evento(String nome_produto, int quantidade, double preço_unitario) {
-        this.nome_produto = nome_produto;
-        this.quantidade = quantidade;
-        this.preço_unitario = preço_unitario;
+    public Evento(String nomeProduto, int quant, double precoUnitario) {
+        this.nome_produto = nomeProduto;
+        this.quantidade = quant;
+        this.preco_unitario = precoUnitario;
     }
 
     public String getNome_produto() {
@@ -17,8 +17,23 @@ public class Evento implements Serializable {
     public int getQuantidade() {
         return quantidade;
     }
-    public double getPreço_unitario() {
-        return preço_unitario;
+    public double getPreco_unitario() {
+        return preco_unitario;
     }
 
+    public String toString() {
+        return "Evento [nome_produto=" + nome_produto + 
+                     ", quantidade=" + quantidade + 
+                     ", preco_unitario=" + preco_unitario + 
+                     "]";
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Evento evento = (Evento) o;
+        return quantidade == evento.quantidade &&
+               Double.compare(evento.preco_unitario, preco_unitario) == 0 &&
+               nome_produto.equals(evento.nome_produto);
+    }
 }
