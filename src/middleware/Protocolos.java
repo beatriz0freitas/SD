8a2233f37bac79453882dd709868717a21760d73
beatriz0freitas <a106853@alunos.uminho.pt>
2025-12-30 16:23:00 +0000

    adicionar constantes em falta

--- src/middleware/Protocolos.java
+++ src/middleware/Protocolos.java
@@ -20,12 +20,14 @@ public final class Protocolos {
     public static final byte EVENTO_NOVO_DIA                      = 3;
     public static final byte EVENTO_NOTIFICAR_VENDA_ESPECIFICA    = 4;
     public static final byte EVENTO_NOTIFICAR_VENDAS_CONSECUTIVAS = 5;
+    public static final byte EVENTO_FILTRAR                       = 6;
 
     // Métodos Agregações
     public static final byte AGREG_QTD            = 1;
     public static final byte AGREG_VOLUME         = 2;
     public static final byte AGREG_PRECO_MEDIO    = 3;
     public static final byte AGREG_PRECO_MAXIMO   = 4;
+    public static final byte AGREG_COMPLETA       = 5;
 
     // Métodos Admin
     public static final byte ADMIN_LISTAR_CLIENTES = 1;
